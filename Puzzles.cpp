4a3da2b6f007ac3bc94276391a791175cc61b43a
Mohamed HellmY <55303037+7elmie@users.noreply.github.com>
2022-12-10 18:07:01 +0200

    337A - Puzzles

---
+++ Puzzles.cpp
@@ -0,0 +1,26 @@
+// 	Dec 10, 2022 06:06:19 PM 		337A - Puzzles 	GNU C++0x 	Accepted 	30 ms 	0 KB
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n, m, f[1000];
+    cin >> n >> m;
+    for (int i = 0; i < m; ++i)
+    {
+        cin >> f[i];
+    }
+    sort(f, f + m);
+    int least = f[n-1] - f[0];
+    for (int i = 1; i <= m - n; ++i)
+    {
+        if (f[i+n-1] - f[i] < least)
+        {
+            least = f[i+n-1] - f[i];
+        }
+    }
+    cout << least << endl;
+    return 0;
+}
