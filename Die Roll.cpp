1ae30abd49295a1696f598fbe7562a801ab9d43d
Mohamed HellmY <55303037+7elmie@users.noreply.github.com>
2022-12-18 00:21:30 +0200

    9A - Die Roll.cpp

---
+++ Die Roll.cpp
@@ -0,0 +1,16 @@
+//   Dec 18, 2022 12:19:44 PM		 9A - Die Roll	 GNU C++0x	Accepted	30 ms	0 KB
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int Y, W;
+    cin >> Y >> W;
+
+    const string probability[7] = {"", "1/1", "5/6", "2/3", "1/2", "1/3", "1/6"};
+    int D = max(Y, W);
+    cout << probability[D] << endl;
+    return 0;
+}
