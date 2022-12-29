87c6700c11320a379def41e51f1cfbb367f8b277
Mohamed HellmY <55303037+7elmie@users.noreply.github.com>
2022-12-29 20:58:53 +0200

    B. Books

---
+++ B. Books.cpp
@@ -0,0 +1,34 @@
+/* 	 Dec 29, 2022 10:00:22 PM		 B. Books	 GNU C++	Accepted	30 ms	200 KB */
+
+#include <cstdio>
+ 
+int main()
+{
+    int n, t, a[100000];
+    scanf("%d%d", &n, &t);
+    for (int i = 0; i < n; ++i)
+    {
+        scanf("%d", &a[i]);
+    }
+ 
+    int j(0), time(0);
+    while (j < n && time + a[j] <= t)
+    {
+        time += a[j++];
+    }
+    int maximum(j);
+    for (int i = 0; i < n; ++i)
+    {
+        time -= a[i];
+        while (j < n && time + a[j] <= t)
+        {
+            time += a[j++];
+        }
+        if (j - i - 1 > maximum)
+        {
+            maximum = j - i - 1;
+        }
+    }
+    printf("%d\n", maximum);
+    return 0;
+}
