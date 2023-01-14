9a3509d548d2c17f072ff2662fb0f4f5193793c2
Mohamed HellmY <55303037+7elmie@users.noreply.github.com>
2023-01-14 09:21:53 -0800

    Update Phone Code.cpp

--- Phone Code.cpp
+++ Phone Code.cpp
@@ -1,9 +1,7 @@
 //4059111   Jan 9, 2023 09:15:39 AM 	 	172A - Phone Code 	GNU C++0x 	Accepted 	31 ms 	0 KB
 #include <cstdio>
 #include <cstring>
-
-int main()
-{
+int main(){
     int n;
     char s0[21], s[21];
     scanf("%d%s", &n, s0);
